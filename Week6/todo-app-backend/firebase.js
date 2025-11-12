7eff0b5e66164f9d396a140a0958ec1a0264e09b
Shriyaa Balaji <shriyaabalaji007@gmail.com>
2025-11-11 20:04:22 -0600

    week 6 hw

--- Week6/todo-app-backend/firebase.js
+++ Week6/todo-app-backend/firebase.js
@@ -18,7 +18,7 @@ const creds = JSON.parse(process.env.FIREBASE_CREDENTIALS);
 // Initializing Firebase Admin SDK with credentials and database URL
 admin.initializeApp({
   credential: admin.credential.cert(creds),
-  databaseURL: "https://tpeo-todo.firebaseio.com",  // TODO: replace with your database URL
+  databaseURL: "https://todoapp-575e0.firebaseio.com",  // TODO: replace with your database URL
 });
 
 const db = admin.firestore();
